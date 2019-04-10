76397e23215ba32ad1c8ef90ac5d62b9aac25ce3
Dmytro Malohlovets <Dmytro_Malohlovets@epam.com>
2019-04-10 15:05:00 +0300

    changes for test trigger local CI #14

--- hooks/TriggerLocalBuildOnCommit.ps1
+++ hooks/TriggerLocalBuildOnCommit.ps1
@@ -1,2 +1,2 @@
-param([String]$folder)
-Invoke-WebRequest -Uri http://localhost:8080/git/notifyCommit?url=file://$folder -Method POST
+param([string]$path)
+Invoke-WebRequest -Uri http://localhost:8080/git/notifyCommit?url=file://$path -Method POST
